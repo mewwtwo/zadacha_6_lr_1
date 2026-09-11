9334b0a97044c0ef87a8deaf1cbf343d71963db9
pharchenko <pharcenko351@gmail.com>
2026-09-11 22:28:14 +0300

    added a calculation function

--- Generator.h
+++ Generator.h
@@ -21,6 +21,24 @@ struct NoMoreVariants : runtime_error {
     NoMoreVariants() : runtime_error("Усі унікальні варіанти вже згенеровано") {}
 };
 
+class Generator {
+    vector<string> q;
+    size_t n;
+
+    set<Variants, less<Variants>> used;
+    mt19937 rng{random_device{}()};
+
+    static unsigned long long C(unsigned long long M, unsigned long long n) {
+        if (n > M) return 0;
+        n = min(n, M - n);
+        unsigned long long r = 1;
+        for (unsigned long long i = 1; i <= n; ++i) {
+            r = r * (M - n + i) / i;
+        }
+        return r;
+    }
+
+};
 // Власний клас функціональних об'єктів
 class Generator {
     vector<string> q;
