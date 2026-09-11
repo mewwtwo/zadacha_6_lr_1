eb3b3a1a9f0cbb5993dde8ed49110959a2f5d52e
Dmytro Demchuk <ddemcuk12@gmail.com>
2026-09-11 20:58:51 +0300

    каркас main.cpp

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	g++ -std=c++23 main.cpp -o zadacha_6_lr_1 # 	 |	macOS
+# g++ -std=c++23 -O2 main.cpp -o zadacha_6_lr_1.exe  |	Windows
--- main.cpp
+++ main.cpp
@@ -1,2 +1,17 @@
-// K-15 Dmytro Demchuk
+// Dmytro Demchuk K-25 & Polina Harchenko K-28
+// GCC 13+ (g++ -std=c++23) | MSVC v19.36+ (/std:c++latest)
+
+#include "Generator.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
+
+static bool fileExists(const string& name) {
+    return ifstream(name).good();
+}
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
