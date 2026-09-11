2aab517dd31461afb8effdf6448137d0a352404d
pharchenko <pharcenko351@gmail.com>
2026-09-11 22:30:40 +0300

    combinatorics

--- Generator.h
+++ Generator.h
@@ -20,3 +20,22 @@ struct Variants {
 struct NoMoreVariants : runtime_error {
     NoMoreVariants() : runtime_error("Усі унікальні варіанти вже згенеровано") {}
 };
+class Generator {
+    vector<string> q;
+    size_t n;
+
+    set<Variants, less<Variants>> used;
+    mt19937 rng{random_device{}()};
+
+    static unsigned long long C(unsigned long long M, unsigned long long n) {
+        if (n > M) return 0;
+        n = min(n, M - n);
+        unsigned long long r = 1;
+        for (unsigned long long i = 1; i <= n; ++i) {
+            r = r * (M - n + i) / i;
+        }
+        return r;
+    }
+
+public:
+};
